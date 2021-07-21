7551a30ede79eeba0ae52f93874885bdb0091dc1
Arisa Hirata <arisa.pirata@gmail.com>
2021-07-20 22:33:52 -0700

    styling weather card using materialUI

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
-const OPEN_WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
+const OPEN_WEATHER_API_KEY = 'e1ec43e71a98cc14a4799aabd7b70c27';
 
-export interface OpenWEatherData {
+export interface OpenWeatherData {
 	name: string
 	main: {
 		feels_like: number
@@ -31,6 +31,6 @@ export async function fetchOpenWeatherData(city: string): Promise<any> {
 		throw new Error('City not found')
 	}
 
-	const data: OpenWEatherData = await res.json()
+	const data: OpenWeatherData = await res.json()
 	return data
 }
